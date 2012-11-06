d82fddfce60a8f3c72ba9c1ac41795c10cc06f61
Tony Butler <tonyjbutler@gmail.com>
2012-11-06 13:41:38 +0000

    Don't exclude any courses that are included in GSB processing from total course count (and avoid 'division by zero' errors)

--- index.php
+++ index.php
@@ -172,7 +172,6 @@ if($config->subcategories == '1') {
 	$totalcourses = $DB->get_records_sql("SELECT {course}.id, {role_assignments}.roleid, Count({role_assignments}.roleid) AS studentsenrolled
 										FROM {user} INNER JOIN (({role_assignments} INNER JOIN {context} ON {role_assignments}.contextid = {context}.id) INNER JOIN ({course} INNER JOIN {course_categories} ON {course}.category = {course_categories}.id) ON {context}.instanceid = {course}.id) ON {user}.id = {role_assignments}.userid
 										GROUP BY {role_assignments}.roleid, {course}.id
-										HAVING ((({role_assignments}.roleid)=5) AND ((Count({role_assignments}.roleid))>= $enrolments))
 										ORDER BY Count({role_assignments}.roleid)");	
 } else {
 
@@ -180,7 +179,6 @@ if($config->subcategories == '1') {
 										FROM {user} INNER JOIN (({role_assignments} INNER JOIN {context} ON {role_assignments}.contextid = {context}.id) INNER JOIN ({course} INNER JOIN {course_categories} ON {course}.category = {course_categories}.id) ON {context}.instanceid = {course}.id) ON {user}.id = {role_assignments}.userid
 										WHERE ((({course_categories}.depth)=1))
 										GROUP BY {role_assignments}.roleid, {course}.id
-										HAVING ((({role_assignments}.roleid)=5) AND ((Count({role_assignments}.roleid))>= $enrolments))
 										ORDER BY Count({role_assignments}.roleid)");
 								 						 
 }							 
